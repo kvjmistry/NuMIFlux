eb38fd8b0473b88b237be85a085c6a6f8e3b662e
Krishan Mistry <kvjmistry@gmail.com>
2019-08-05 13:16:15 -0500

    Adding more grid files and updated the plot constraints script

--- PPFX_Constraints/plots/plot_constraints.C
+++ PPFX_Constraints/plots/plot_constraints.C
@@ -13,8 +13,6 @@ void plot_constraints(){
     boolfile  = GetFile(f , "../NuMIFlux.root"); 
     if (boolfile == false) gSystem->Exit(0);
 
-   
-
     std::vector<std::string> pars =  {
         "pionplus_NA49",
         "pionminus_NA49",
@@ -41,7 +39,6 @@ void plot_constraints(){
         DrawSpecifiers(hist, pars.at(k));
         IncreaseLabelSize(hist);
         gStyle->SetTitleH(0.07);
-        
         
         std::vector<TLine*> lineVector = GetLineVector(pars.at(k));
 
